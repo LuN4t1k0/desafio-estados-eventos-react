1c46b67feffeab8e33821f7dc8e34d11fd8dad6e
Cristian Venegas <cristian.venegas@materoceano.com>
2022-08-19 17:15:54 -0400

    style : add hidden class

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,20 +1,34 @@
-import {useState} from "react";
+import { useState } from "react";
 import Boton from "./Boton";
 
-const Formulario = ({password, setPassword, nombre, setNombre}) => {
-
-
+const Formulario = ({ password, setPassword, nombre, setNombre }) => {
   return (
     <form action="https://pranx.com/hacker/simulador/">
-      <div className="form-group">
+      <div className={ password === "252525" ? "hidden" : "form-group"}>
         <label htmlFor="nombre">Nombre</label>
-        <input type="text" name="nombre" id="nombre" onChange={e => setNombre(e.target.value)}/>
+        <input
+          type="text"
+          name="nombre"
+          id="nombre"
+          onChange={(e) => setNombre(e.target.value)}
+        />
       </div>
-      <div className="form-group">
+      <div className={ password === "252525" ? "hidden" : "form-group"}>
         <label htmlFor="password">Contraseña</label>
-        <input type="password" name="password" id="password" onChange={(e) => setPassword(e.target.value)}/>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
-    {password === "252525" ? <div><h1>Bienvenido {nombre}</h1> <Boton/> </div>  : <p>ingresa una contraseña</p>}
+      {password === "252525" ? (
+        <div>
+          <h1>Bienvenido {nombre}</h1> <Boton />{" "}
+        </div>
+      ) : (
+        <p>ingresa una contraseña</p>
+      )}
     </form>
   );
 };
